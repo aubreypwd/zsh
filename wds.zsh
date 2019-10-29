403751da10531db44780dfc077d86afcac3fd246
Aubrey Portwood <code@aubreypwd.com>
2019-10-29 07:33:41 -0700

    blogmule, so much easier now.

--- wds.zsh
+++ wds.zsh
@@ -12,3 +12,14 @@
 function satisbuild {
 	ssh root@packages.wdslab.com "sh /srv/global/packages/rebuildsatis.sh"
 }
+
+###
+ # I never have to remember this IP again!
+ #
+ # E.g: blogmule
+ #
+ # @since Tuesday, 10/29/2019
+ ##
+function blogmule {
+	ssh blogmule@40.83.223.82
+}
