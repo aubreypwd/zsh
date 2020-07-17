93bdfd255158fde7a2eb06a6c0677eac7cd22f20
Aubrey Portwood <code@aubreypwd.com>
2020-07-16 22:18:43 -0600

    Don't do this every time.

--- docker.zsh
+++ docker.zsh
@@ -1,4 +1,4 @@
 #!/usr/local/bin/zsh
 
 # https://stackoverflow.com/questions/37599128/docker-how-do-you-disable-auto-restart-on-a-container
-docker update --restart=no $(docker ps -a -q) &> /dev/null
+#docker update --restart=no $(docker ps -a -q) &> /dev/null
